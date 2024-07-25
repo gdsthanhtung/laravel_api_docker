65668b22fc6bf93b0d5a918cb460fe2d7de751d9
“GdsThanhTung” <gds.thanhtung@gmail.com>
2024-07-25 10:03:27 +0700

    JWT Auth guard by Tymon lib

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::resource('products', ProductController::class);
+Route::group([
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+    Route::get('/profile', [AuthController::class, 'userProfile'])->middleware('auth:api');
+    Route::post('/change-pass', [AuthController::class, 'changePassWord'])->middleware('auth:api');
+});
+
+
+Route::resource('products', ProductController::class)->middleware('auth:api');
